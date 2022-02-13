2dcbf25925cfd62a2ba297628623b30ffe9548c7
Doug Smith <superdug@gmail.com>
2022-02-13 07:28:56 -0600

    test

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "aws_s3_bucket" "emailBucket" {
 }
 
 resource "aws_s3_bucket_acl" "emailBucket" {
-  bucket     = aws_s3_bucket.emailBucket.id 
+  bucket = aws_s3_bucket.emailBucket.id
   acl = "private"
 }
 
